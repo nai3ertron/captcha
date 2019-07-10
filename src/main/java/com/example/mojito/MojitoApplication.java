c88c779e70d9514bf2db3109b6a3b8cbfe15a84d
sukan <sukan.stbsr@gmail.com>
2019-07-10 20:36:32 +0700

    work shop

---
+++ src/main/java/com/example/mojito/MojitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mojito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojitoApplication.class, args);
+	}
+
+}
